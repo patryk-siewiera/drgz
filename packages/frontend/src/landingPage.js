2384e1e8dd20cd08c38af42903b3ffb8aa5077b0
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-05-22 15:36:38 +0200

    feat: styling tailwind (gradients)

--- packages/frontend/src/landingPage.js
+++ packages/frontend/src/landingPage.js
@@ -2,33 +2,35 @@ import droneOperators from "./img/droneOperators.png"
 
 function LandingPage() {
   return (
-    <div className="app">
+    <div className="app font-thin text-2xl">
       <link
         href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
         rel="stylesheet"
       ></link>
-      <div className="p-10">
-        <div className="md:flex bg-gray-300 rounded-xl p-8 md:p-20 ">
+      <div className="pt-10 pl-10 pr-10">
+        <div className="bg-gradient-to-r from-red-100 to-blue-200 rounded-xl p-8 md:p-20 text-center">
+          <p className="text-5xl">
           Dronowa giełda zleceń
+          </p>
         </div>
-        <div className="grid gap-x-8 gap-y-4 grid-cols-3 md:p-10">
-          <div className="bg-blue-200 rounded-xl p-5">
+        <div className="grid gap-x-8 gap-y-4 grid-cols-3 md:pt-10 text-center">
+          <div className="bg-blue-100 rounded-xl p-5">
             Współpracujemy tylko z najlepszymi
           </div>
           <div className="bg-blue-200 rounded-xl p-5">
             Używamy tylko najlepszego sprzętu
           </div>
-          <div className="bg-blue-200 rounded-xl p-5">
+          <div className="bg-blue-300 rounded-xl p-5">
             Doświadczenie i renoma
           </div>
         </div>
 
         <div className="grid gap-x-8 gap-y-4 grid-cols-1 md:p-10">
-          <div className="bg-red-100 rounded-md p-5">1. Zarejestruj się</div>
-          <div className="bg-red-100 rounded-md p-5">
+          <div className="bg-green-100 rounded-md p-7 ">1. Zarejestruj się</div>
+          <div className="bg-green-200 rounded-md p-7">
             2. Dodaj zlecenie (inspekcja, raport UDT)
           </div>
-          <div className="bg-red-100 rounded-md p-5">
+          <div className="bg-green-300 rounded-md p-7">
             3. Znajdź wykonawcę z odpowiednim dronem w Twoim województwie
           </div>
         </div>
