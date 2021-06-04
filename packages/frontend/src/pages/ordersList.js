0c8558cc87e818c70a48ae2894c274f680ce4b08
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-06-04 23:47:27 +0200

    feat: quickly add a new component

--- packages/frontend/src/pages/ordersList.js
+++ packages/frontend/src/pages/ordersList.js
@@ -7,13 +7,16 @@ import {
   Link,
   Redirect,
 } from "react-router-dom";
-import CardOrder from "./CardOrders";
-
-function showAlert() {
-  alert("test");
-}
+import CardOrder from "./components/CardOrders";
+import { createContext, useState, Component } from "react";
 
 function OrdersList() {
+  const [drafts, setDrafts] = useState([]);
+
+  function createNewEntry() {
+    setDrafts([...drafts, <CardOrder />]);
+  }
+
   return (
     <div>
       <div className="items-center p-20 m-auto mw-w-md max-w-7xl">
@@ -32,27 +35,26 @@ function OrdersList() {
             </button>
           </Link>
         </div>
+
         <br />
         <br />
+
         <button
-          onClick={showAlert}
-          className="p-5 font-semibold text-center bg-red-100 fo0t-semibold right-1 rounded-xl hover:bg-blue-200"
+          onClick={createNewEntry}
+          className="p-5 font-semibold text-center bg-red-100 right-1 rounded-xl hover:bg-blue-200"
         >
-          quickly add new component
+          quickly add a new component
         </button>
 
         <br />
         <br />
         <br />
         <br />
+
         <div className="grid grid-cols-4 gap-4">
-          <CardOrder /> 
           <CardOrder />
           <CardOrder />
-          <CardOrder />
-          <CardOrder />
-          <CardOrder />
-          <CardOrder />
+          {drafts}
         </div>
         <br />
       </div>
