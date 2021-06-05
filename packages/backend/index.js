d7447123ada51c3b7c17aa3fef03d4454b62f537
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-06-05 21:45:52 +0200

    feat: retrieve orders from the db via prisma orm

--- packages/backend/index.js
+++ packages/backend/index.js
@@ -1,9 +1,22 @@
+const PrismaClient = require("@prisma/client").PrismaClient;
+
 const express = require("express");
 const app = express();
 const port = 3001;
 
-app.get("/", (req, res) => {
-  res.send("Hello World, from express");
+const prisma = new PrismaClient();
+
+app.get("/order", async (req, res) => {
+  const orders = await prisma.orders.findMany();
+  console.info("orders", orders);
+  res.send(orders);
+});
+
+app.post("/order", async (req, res) => {
+  const order = JSON.parse(req.body);
+  console.info("order", order);
+  await prisma.orders.create({ data: order });
+  res.send("");
 });
 
 app.listen(port, () =>
