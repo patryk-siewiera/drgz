ecd585ffd93dc6eee8f0247bcb0fda587050813f
Patryk Siewiera <45758580+patryk-siewiera@users.noreply.github.com>
2021-07-05 14:10:36 +0200

    feat: description max length 200 chars

--- packages/frontend/src/pages/addOrder.js
+++ packages/frontend/src/pages/addOrder.js
@@ -102,9 +102,15 @@ function AddOrder() {
 							{" "}
 							Opis
 							<input
-								{...register("description", { required: true })}
+								{...register("description", {
+									required: true,
+									maxLength: 200,
+								})}
 								className="block w-full px-4 py-3 leading-tight text-gray-700 bg-gray-100 border border-gray-200 rounded appearance-none focus:outline-none focus:bg-white focus:border-gray-500"
 								type="text"
+								aria-invalid={
+									errors.description ? "true" : "false"
+								}
 								id="description"
 								placeholder="Opis zlecenia"
 							/>
@@ -123,12 +129,19 @@ function AddOrder() {
 								type="number"
 								placeholder="1000"
 							/>
+						</label>
+						<div className="errors">
 							{errors.cost && (
-								<div className="font-bold text-red-500 underline">
+								<div className="mt-5 mb-5 font-bold text-red-500 underline">
 									Cena nie może być ujemna
 								</div>
 							)}
-						</label>
+							{errors.description && (
+								<div className="mt-5 mb-5 font-bold text-red-500 underline">
+									Opis jest większy niż 200 znaków
+								</div>
+							)}
+						</div>
 						<label>
 							<input
 								type="checkbox"
